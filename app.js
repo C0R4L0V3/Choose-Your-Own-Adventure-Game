ff49060e62f990786febaa77566fca8d36ae4852
C0R4L0V3 <coraline.e.love@gmail.com>
2024-10-17 11:12:25 -0500

    this is the most up to date, version for terminal adventure game.

--- app.js
+++ app.js
@@ -1,61 +1,120 @@
+// Terminal game practice
+
 const prompt = require('prompt-sync')();
 
-let playerGold = 100;
+let playerGold = 0;
+let churchGold = 110;
 
 const username = prompt('What is your name? ');
-console.log(`Your name is ${username}`);
+console.log(`
+    Your name is ${username}`);
 
-const startingArea = prompt(`Where would you like to start? field forest town `);
+//function recalls the field direction choices
+function field() {
+    console.log("You are in a field")
+    console.log("To the East of you there is path to a dark forest.")
+    console.log("To the South there is a path to a town.")
+    console.log("To the West there is an old church.")
+    console.log("To the North there is a path to the mountains.")
+    const fieldDirection = prompt(`Which direction would you like to go? North East South West`)
+    return fieldDirection;
+}
 
-if (startingArea === `field`){
-    console.log(`
-        You wake up under a blue sky in a field. You stand up looking around trying to gather your bearings.`);
-    const fieldDirection = prompt(`
-        To the East of you there is path to a dark forest. 
-        To the South there is a path to a town. 
-        To the West there is an old church. 
-        And to the North there is a path to the mountains. 
-        Which direction would you like to go? `);
-    if (fieldDirection == `south`){
-            console.log(`You find yourself in the town of Honeywood, there is a blacksmith's and an Inn.`);
-            const honeyWood = prompt(`Where would you like to go? 
-                `);
-                // }
-                // if (honeyWood === `blacksmith` && playerGold >= 200){
-                //     console.log(`the blacksmith has a sword for sale for 200 gold and have enough gold it. ${playerGold} `)
-                // } else (honeyWood === `blacksmith` && playerGold <= 200){
-                //     console.log(`The blacksmith has a sowrd for sale 200 gold, but you do not have enough to buy it. ${playerGold}`)
-                // }
-        } else if (fieldDirection == `east`){
-            console.log(`
-                Following the path East you find yourself in a dark forest. 
-                The path East continues to go deeper into the forest. 
-                The path West continues back to the field.`);
-            const forestPath = prompt(`which direction do you want to go down? `);
-                // if (forestPath == `west`) {
-                //     console.log("You reach the edge of the forest and find yourself in a field")
-                // } else if (forestPath == `east`){
-                //     console.log("You continue down east and find a dark cave")
-                // }
-        }
+// function recalls the forest driection choices
+function forest() {
 
-    } else if (startingArea === `forest`){
-    console.log(`
-        You are traveling down a dark path in a forest. 
-        As you travel west down the path you see the edge of the forest with a field on the horizon. 
-        The path East continues deeper into the forest. `);
-    const forestPath = prompt(`which direction do you want to go down? `);
-        if (forestPath == `west`) {
-            console.log(`
-                You reach the edge of the forest and find yourself in a field`);
-        } else if (forestPath == `east`){
-            console.log(`
-                You continue down east and find a dark cave`);
-        }
+    console.log("You are traveling down a dark path in a forest.")
+    console.log("The path East continues to go deeper into the forest.")
+    console.log("The path West continues back to the field.")
+    const forestDirection = prompt(`Which direction do you want to go down? East West`)
+    return forestDirection;
+}
 
-    } else if (startingArea == `town`){
-    console.log(`this is a test`)
+function forestBear() {
+    console.log("You continue down east and find a dark cave.")
+    console.log("A bear has killed you.")
+}
+
+// function function if the player does not have enough gold to stay at the inn
+function honeyWoodInnPoor() {
+    console.log(`It costs 5 gold to stay at the inn, you do not have enough gold. Your gold:${playerGold}`)
+    console.log("You do not have enough gold you peasent! *you have been kicked out of the Inn* ")
+
+}
+
+//function if player does not have enough gold for the blacksmith
+function honeyWoodBlacksmithPoor() {
+    console.log(`The blacksmith has a sword for sale 200 gold, but you do not have enough to buy it. Your gold:${playerGold}`)
+    console.log(`"Get out of my shop you beggar!" *You have been kicked out of the Blacksmith's*`)
+
 }
 
 
 
+function churchGoldAdd (){
+    console.log(playerGold+churchGold)
+
+}
+
+
+const startingArea = prompt(`Where would you like to start? field forest town `); //starting place choice
+
+// Starting paths for field choice
+if (startingArea === `field`) {
+    console.log(`You wake up under a blue sky in a field. You stand up looking around trying to gather your bearings.`)
+    const fieldDirection = field(); // envokes the field funtion prompt
+
+    if (fieldDirection === `south`) {
+        console.log(`You find yourself in the town of Honeywood, there is a blacksmith's and an Inn.`);
+        const honeyWood = prompt('Where would you like to go? Blacksmith or Inn')
+
+        if (honeyWood == 'blacksmith' && playerGold >= 200) {
+            console.log(`The blacksmith has a sword for sale for 200 gold and have enough gold it. ${playerGold} `)
+        } else if (honeyWood === `blacksmith` && playerGold <= 200) {
+            const honeyWood = honeyWoodBlacksmithPoor() // envokes the honeyWoodBlacksmithPoor function
+        } else if (honeyWood === 'inn' && playerGold >= 5) {
+            console.log(`You can stay the night for 5 gold. Your gold:${playerGold}`)
+        } else if (honeyWood === 'inn' && playerGold < 5) {
+            const honeyWood = honeyWoodInnPoor() // envokes the honeyWoosInnPoor function
+        }
+
+    } else if (fieldDirection == `east`) {
+        const forestDirection = forest();
+        if (forestDirection == `west`) {
+            console.log("You reach the edge of the forest and find yourself in a field")
+            const forestDirection = field();
+        } else if (forestDirection == `east`) {
+            const forestDirection = forestBear();
+        }
+
+    } else if (fieldDirection == `west`){
+        console.log('You have discovered an old abandoned Church') // could be consolidated to a function
+        const oldChurch = prompt('Would you like to explore or leave? ')
+            if(oldChurch === `explore`){ 
+                console.log("You found a chest with 110 gold!")
+                const gold = churchGoldAdd()
+                console.log("You are now leaving the oldChurch")
+                const fieldDirection = field()
+
+            } else if (oldChurch == `leave`){
+                const church = field();
+            }    
+    } else if (fieldDirection == 'north'){
+
+    }
+
+    // Starting paths for forest choice
+} else if (startingArea === `forest`) {
+    const forestDirection = forest();
+
+
+    if (forestDirection == `west`) {
+        const fieldDirection = field()
+    } else if (forestDirection == `east`) {
+        const forestDirection = forestBear();
+    }
+
+    // Starting paths for town choice
+} else if (startingArea === `town`) {
+    console.log(`this is a test`)
+}
